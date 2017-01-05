bf898312eed58ae61e3cee2afa7883b19eadf933
punsith <1234apunsith@gmail.com>
2017-01-04 20:55:26 -0500

    Update Archiver.h

--- Source/Archiver.h
+++ Source/Archiver.h
@@ -2,21 +2,21 @@
 
 Copyright (c) 2002 Marcin 'Shard' Konicki
 
-Permission is hereby granted, free of charge, to any person obtaining a copy 
-of this software and associated documentation files (the "Software"), to deal 
-in the Software without restriction, including without limitation the rights 
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
-copies of the Software, and to permit persons to whom the Software is 
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
-The above copyright notice and this permission notice shall be included in all 
+The above copyright notice and this permission notice shall be included in all
 copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE
 
 */
@@ -51,7 +51,7 @@ SOFTWARE
 #include <View.h>
 #include <Window.h>
 
-#include <be/add-ons/tracker/TrackerAddon.h>
+#include </boot/home/haiku/headers/os/add-ons/tracker/TrackerAddOn.h>
 
 //----------------------------------------------------------------------------
 //
@@ -213,9 +213,9 @@ class ArchiverWindow : public BWindow
 		void			ChangeSettings( BMessage *settings);
 		void			Compress( BMessage *refs);
 		void			Reorganize();
-		
+
 		BMessage		*GetSettings() { return aSettings; };
-		
+
 	private:
 		void			LoadSettings();
 		void			LoadDefaultSettings();
